58c4662042b1b3b317c99d8e3481c59e8f35a270
as21507-art <as21507@nyu.edu>
2025-11-23 01:10:26 +0400

    Constants added for fiducial markers

--- globals.py
+++ globals.py
@@ -13,6 +13,9 @@ default_tank_radius = 25
 default_bullet_size = 10
 default_bullet_range = 100
 default_bullet_damage = 20
+default_bullet_max_velocity = 20
+
+drag_limit = 200
 
 p1_limit = [100, 700]
 p2_limit = [1000, 1600]
@@ -23,3 +26,6 @@ health_bar_width = 150
 health_bar_height = 15
 
 gravity_effect = 4
+
+fiducial_scope = 4
+fiducial_line = 10
