4b6cc73cdd3dacf8331671aa018a6ddf0320540d
as21507-art <as21507@nyu.edu>
2025-12-01 20:37:20 +0400

    Adjust bullet movement speed and comments
    Updated bullet movement speed and clarified comments.

--- bullet.py
+++ bullet.py
@@ -5,21 +5,22 @@ class Bullet:
 
 	def __init__(self, player, tank_x, tank_y, tank_radius, velocity, angle, mode = "default"):
 		angle = math.radians(angle)
-		self.direction = math.pow(-1, player + 1)					# Since they are facing opposite directions, x values will be either incremented or decremented
+		self.direction = math.pow(-1, player + 1)						# For player 1, X values are incremented (+1) and for player 2, X values are decremented (-1)
 		self.x = int(tank_x + self.direction * math.cos(angle) * (tank_radius + globals.default_tank_nozzle))    	# Initial x coordinate of the bullet
-		self.y = int(tank_y - math.sin(angle) * (tank_radius + globals.default_tank_nozzle))						# Initial y coordinate of the bulelt
+		self.y = int(tank_y - math.sin(angle) * (tank_radius + globals.default_tank_nozzle))						# Initial y coordinate of the bullet
 		self.vx = velocity * math.cos(angle)                       		# Initial horizontal velocity of the bullet
 		self.vy = - velocity * math.sin(angle)                       	# Initial vertical velocity of the bullet
-		self.r = globals.default_bullet_size // 2                       # Raidus of the bullet
+		self.r = globals.default_bullet_size // 2                       # Radius of the bullet
 		self.landed = False                                          	# Flag to check if the bullet has landed
 		self.range = globals.default_bullet_range
 		self.damage = globals.default_bullet_damage
 		# Add bullet image
 
+	# Applies gravity only to the vertical velocity
 	def gravity(self):
-		self.vy = self.vy + globals.gravity_effect							# Applies gravity only to the vertical velocity
+		self.vy = self.vy + globals.gravity_effect
 
-	# Handles the case when the bullet attemps to go below the ground
+	# Handles the case when the bullet attempts to go below the ground
 	def adjust_landing(self):
 		self.x = self.x + self.direction * self.vx * (globals.ground_level - self.y) / self.vy    	# Linearly approximates the horizontal position
 		self.y = globals.ground_level - self.r 														# Rests the vertical position to ground level
@@ -37,7 +38,7 @@ class Bullet:
 	# Moves the bullet if it has not yet landed
 	def move(self):
 		if not self.landed:
-			if frameCount % 8 == 0:										# Slows down the animation of bullet
+			if frameCount % 30 == 0:									# Slows down the animation of bullet
 				self.follow_trajectory()
 		return self.x, self.y
 
@@ -52,7 +53,6 @@ class Bullet:
 
 	# Displays the bullet at its current coordinates
 	def display(self):
-		# Display image
 		stroke(0)
 		fill(0)
 		ellipse(int(self.x), int(self.y), 2 * self.r, 2 * self.r)
