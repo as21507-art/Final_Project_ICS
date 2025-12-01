5168ed27c3bfcf246ee8c9bb67958f0452c0c8c2
as21507-art <as21507@nyu.edu>
2025-12-01 20:39:24 +0400

    Update global variables for game configuration

--- globals.py
+++ globals.py
@@ -14,39 +14,40 @@ default_tank_nozzle = 35
 default_bullet_size = 10
 default_bullet_range = 100
 default_bullet_damage = 20
-default_bullet_max_velocity = 50
-default_bullet_min_velocity = 4
-bullet_velocity_min_factor = default_bullet_min_velocity / default_bullet_max_velocity
+default_bullet_max_velocity = 100
+bullet_velocity_min_factor = 0.2
 
-drag_limit = 200
+drag_limit = 300
 
 p1_limit = [100, 700]
 p2_limit = [1000, 1600]
 p1_start_x = 400
 p2_start_x = 1200
 
-horizontal_health_bar_position = [100, 700]
+horizontal_health_bar_position = [150, 1550]
 vertical_health_bar_position = 100
 health_bar_width = 150
 health_bar_height = 15
 
-gravity_effect = 4
+gravity_effect = 8
 
 fiducial_scope = 4
 fiducial_line = 10
 
-play_button = [250, 80]
+play_button = [270, 75]
 play_position = 0.6
 other_position = 0.7
-tutorial_button = [250, 60]
-quit_button = [120, 60]
-mute_button = [60, 60]
+tutorial_button = [200, 50]
+quit_button = [200, 50]
+resume_button = [200, 50]
+exit_button = [200, 50]
+mute_button = [50, 50]
+cancel_button = [50, 50]
 button_spacing = 30             # Half of the distance between two buttons
 
 quit_pop_up = [60, 60]
 pause_pop_up = [90, 90]
 tutorial_pop_up = [100, 100]
 
-logo_width = 450
+logo_width = 500
 logo_height = 400
-
