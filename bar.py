c98ca03ef7549cc185c1340b448c3e5904af8c87
as21507-art <as21507@nyu.edu>
2025-12-08 12:49:57 +0400

    Refactor Health and Power bar classes
    Refactor health and power bar classes, consolidating common functionality into a base class.

--- bar.py
+++ bar.py
@@ -1,91 +1,103 @@
 import globals
 
-class Health:
+class Bar:
 
-	def __init__(self, player):
-		self.player = player															# Current player, 1 or 2
-		self.w = globals.health_bar_width												# Width of health bar
-		self.h = globals.health_bar_height												# Height of health bar
-		self.x = globals.horizontal_health_bar_position[player - 1] - self.w // 2 		# Converting centre x to upperleft x
-		self.y = globals.vertical_health_bar_position - self.h // 2 					# Converting centre y to upperleft y
-		self.health = 100																# Stores remaining health of the player (0 to 100)
-		self.colour = [103, 235, 52]													# Stores the colour of health bar
-		self.animate = 0																# Variable that regulates the animation of changing health
+	def __init__(self, player, x, y, w, h):
+		self.player = player											# Current player, 1 or 2
+		self.w = w														# Width of bar
+		self.h = h														# Height of bar
+		self.x = x														# Centre X
+		self.y = y				 										# Centre Y
+		self.upper_x, self.upper_y = self.get_upper_left()				# Converting center of bar to upper_left corners
+		self.colour = [103, 235, 52]									# Stores the colour of bar
+
+	def get_upper_left(self):
+		return self.x - self.w //2, self.y - self.h //2
 
 	# Draws the rectangle
-	def make_health_bar(self):
-		fill(self.colour[0], self.colour[1], self.colour[2])  							# Setting the colour of the health bar
+	def make_bar(self, parameter):
 		noStroke()
-		rect(self.x, self.y, int(self.w * self.health / 100), self.h)  					# Fills remaining health
+		fill(255)
+		rect(self.upper_x, self.upper_y, self.w, self.h)
+		fill(self.colour[0], self.colour[1], self.colour[2])  							# Setting the colour of the bar
+		rect(self.upper_x, self.upper_y, int(self.w * parameter/ 100), self.h)  		# Fills only a portion of the rectangle
 		noFill()
 		stroke(0)
-		strokeWeight(2)
-		rect(self.x, self.y, self.w, self.h)											# Outline of the health bar
+		strokeWeight(1)
+		rect(self.upper_x, self.upper_y, self.w, self.h)								# Outline of the bar
 
 	# Draws the rectangle and updates the colour
-	def display(self):
-		self.make_health_bar()
-		self.animate_colour()
+	def display(self, parameter):
+		self.make_bar(parameter)
 
 	# Linearly transitions between two colours
-	def interpolate(self, colour_1, colour_2,lower):
+	def interpolate(self, colour_1, colour_2,lower, parameter):
 		for i in range(3):
-			self.colour[i] = int(colour_2[i] - ((self.health - lower)/25)*(colour_2[i] - colour_1[i]))
+			self.colour[i] = int(colour_2[i] - ((parameter - lower)/25)*(colour_2[i] - colour_1[i]))
 
 	# Chooses the boundaries colour
-	def change_colour(self):
-		if self.health >= 75:
+	def change_colour(self, parameter):
+		if parameter >= 75:
 			temp_2 = [51, 254, 51]
 			temp_1 = [153, 204, 0]
 			lower = 75
-		elif self.health >=50:
+		elif parameter >=50:
 			temp_2 = [153, 204, 0]
 			temp_1 = [180, 204, 0]
 			lower = 50
-		elif self.health >=25:
+		elif parameter >=25:
 			temp_2 = [180, 204, 0]
 			temp_1 = [179, 179, 0]
 			lower = 25
-		elif int(self.health) > 0:
+		elif parameter >= 0:
 			temp_2 = [179, 179, 0]
 			temp_1 = [180, 41, 0]
 			lower = 0
 		else:
-			self.health = 0
-			return None							# No interpolation of colour when health is 0
-		self.interpolate(temp_2, temp_1, lower)
-			
-	# Decrements health quickly in the begining and slowly towards the end
+			return 0							# No interpolation for out of range values
+		self.interpolate(temp_2, temp_1, lower, parameter)
+
+
+class Health(Bar):
+
+	def __init__(self, player):
+		Bar.__init__(self, player, globals.horizontal_health_bar_position[player - 1], globals.vertical_health_bar_position, globals.bar_width, globals.bar_height)
+		self.health = 100																# Stores remaining health of the player (0 to 100)
+		self.animate = 0  																# Variable that regulates the animation of changing health
+
+	# Decrements health quickly in the beginning and slowly towards the end
 	def animate_colour(self):
 		if int(self.animate) != 0:
 			self.animate = self.animate - (1 + self.animate / 20)			# Non-linear, decreasing speed
 			self.health = self.health - (1 + self.animate / 20)				# Decrements health too
 		else:
-			self.animate = 0     		# Resets the animate to 0
-		self.change_colour()			# Changes the colour of health bar if required
+			self.animate = 0     											# Resets the animate to 0
+		self.change_colour(self.health)										# Changes the colour of health bar if required
 
 	# Initialised the value of animate so that it triggers animate colour
 	def change_health(self, damage):
 		self.animate = damage
-	
 
-# class Power:
+	# Changing the health bar dynamically while being displayed
+	def display(self):
+		Bar.display(self, self.health)
+		self.animate_colour()
 
-# 	def __init__(self, player):
-# 		self.x = globals.horizontal_power_bar_position[player - 1]
-# 		self.y = globals.vertical_power_bar_position
-# 		self.w = globals.power_bar_width
-# 		self.h = globals.power_bar_height
-# 		self.health = 100
-# 		self.colour = [103, 235, 52]
-# 		self.power = 0
 
-# 	def display(self):
-# 		noFill()
-# 		rect(self.x, self.y, self.w, self.h)							# Outline of the health bar
-# 		fill(self.colour[0], self.colour[1], self.colour[2])			# Setting the colour of the health bar
-# 		rect(self.x, self.y, int(self.w * self.power / 100), self.h)	# Remaining health as colour
+class Power(Bar):
 
-# 	# Converts velocity into scale of 0 to 100, where 0 is the min velocity and 100 is the max velocity 
-# 	def change_power(self, velocity):
-# 		self.power = int(100 * (velcoity - globals.default_bullet_min_velocity)/ (globals.default_bullet_max_velocity - globals.default_bullet_min_velocity)
+	def __init__(self, player):
+		Bar.__init__(self, player, globals.horizontal_power_bar_position[player - 1], globals.vertical_power_bar_position, globals.bar_width, globals.bar_height)
+		self.current_power = 0													# Current velocity of the player as compared to the max possible velocity
+
+	# Changes the value of based on the argument passed
+	def change_level(self, velocity):
+		self.current_power = int(100 * velocity / globals.default_bullet_max_velocity)
+		self.change_colour(100 - self.current_power)
+
+	# Dislays the power bar with a label
+	def display(self):
+		fill(0)
+		textAlign(CENTER, CENTER)
+		text("Power", self.x, self.y - 30)
+		Bar.display(self, self.current_power)
